b4fffcb6598d799a99ef6928c084a3fdac04ac6b
N1102393 <tobi.alabi2024@my.ntu.ac.uk>
2025-10-31 14:54:24 +0000

    update urls

--- student_app/student_app/urls.py
+++ student_app/student_app/urls.py
@@ -1,18 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
-from django.shortcuts import redirect
-=======
->>>>>>> 3acf5cfe946606ae718da517462e257cee0fb593
 
 urlpatterns = [
     path('', lambda request: redirect('student/input/')),
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('student/', include('student_colab.urls')),
-=======
     path('', include('student_colab.url')),
 
->>>>>>> 3acf5cfe946606ae718da517462e257cee0fb593
 ]
