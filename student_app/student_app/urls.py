63560f7a5c3e04f3f2e06c16ef17520b5c684c2d
Rexford <rexfordocansey@gmail.com>
2025-10-31 15:42:30 +0000

    working

--- student_app/student_app/urls.py
+++ student_app/student_app/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('', lambda request: redirect('student/input/')),
     path('admin/', admin.site.urls),
-    path('', include('student_colab.url')),
-
+    path('student/', include('student_colab.urls')),
 ]
