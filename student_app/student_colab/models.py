c68ca53dbf309a74f7754cfd555a7c22666f23e7
Rexford <rexfordocansey@gmail.com>
2025-10-31 12:01:23 +0000

    student class start

--- student_app/student_colab/models.py
+++ student_app/student_colab/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    enrollment_date = models.DateField()
+    
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
