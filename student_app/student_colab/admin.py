7f0867a92afe945b093c8ffa24ec9ba6309db884
N1102393 <tobi.alabi2024@my.ntu.ac.uk>
2025-10-31 17:32:15 +0000

    new admin update

--- student_app/student_colab/admin.py
+++ student_app/student_colab/admin.py
@@ -7,7 +7,7 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'student_id', 'email', 'stu_year', 'module')
     search_fields = ('first_name', 'last_name', 'student_id', 'email')
     list_filter = ('module', 'stu_year')
-
+    
 class ModuleAdmin(admin.ModelAdmin):
     list_display = ('module_code', 'module_name')
     search_fields = ('module_code', 'module_name')
@@ -23,15 +23,8 @@ class StuMatchAdmin(admin.ModelAdmin):
     list_display = ('student1', 'student2', 'module', 'status', 'created_at')
     search_fields = ('student1__first_name', 'student1__last_name', 'student2__first_name', 'student2__last_name', 'module__module_code')
     list_filter = ('status', 'module')
-    fieldsets = [
-        ("Match Details", {
-            'fields': ['student1', 'student2', 'module']
-        }),
-        ("Status Information", {
-            'fields': ['status', 'created_at']
-        })
-    ]
+    
 
-admin.site.register(Student)
-admin.site.register(Module)
-admin.site.register(stu_match)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Module, ModuleAdmin)
+admin.site.register(stu_match, StuMatchAdmin)
