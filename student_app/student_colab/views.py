2e21739800b32ec7c9ce27dc71049a15f2dc7f7b
Rexford <rexfordocansey@gmail.com>
2025-10-31 16:33:32 +0000

    should be working, minor errors fixed

--- student_app/student_colab/views.py
+++ student_app/student_colab/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 def get_user_input(request):
     if request.method == 'POST':
         # Get the user input from the form
         user_firstname = request.POST.get('user_firstname')
-        # Do something with the input
+        # Create a new student (you'll need to add more fields to your form)
+        # Student.objects.create(first_name=user_firstname)
         return HttpResponse(f"You entered: {user_firstname}")
     
     # If it's a GET request, show the form
