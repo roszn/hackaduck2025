4a09711b50764fc7680222e12445375f6ac2476d
mwenya <mwenyamail877@gmail.com>
2025-11-01 02:27:09 +0000

    Changed defaul view

--- student_app/student_colab/urls.py
+++ student_app/student_colab/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('input/', views.get_user_input, name='get_user_input'),
     path('', views.home, name='home'),  # Handles the root URL
     path('sign_in/', views.sign_in, name='sign_in'),
     path('profile/', views.profile, name='profile'),
+     path('input/', views.get_user_input, name='get_user_input')
 ]  
